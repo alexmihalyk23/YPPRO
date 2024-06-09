5cceca4cc3375e60bdffc3e3dc0bb15c0ce7e7e2
Egor Romanchenko <egor.gmag@yandex.ru>
2024-06-10 02:33:48 +0700

    Update test_bot.py

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -27,27 +27,28 @@ def bot():
 @patch('main.extract_chat_data')
 def test_bot_message(mock_extract_chat_data, MockScreenManager, MockFile, bot):
     with tempfile.TemporaryDirectory() as temp_dir:
-        mock_extract_chat_data.return_value = (123, '/start')
-        mock_screen = MockScreenManager.return_value
-        mock_screen.current_screen.return_value.run.return_value = [("Welcome to the bot!", None)]
-        mock_screen.current_screen.return_value.next_screen_name = 'next_screen'
-
-        user = User(1, False, 'TestUser')
-        chat = Chat(1, 'private')
-        message = Message(
-            message_id=1,
-            from_user=user,
-            date=None,
-            chat=chat,
-            content_type='text',
-            options={},
-            json_string={'text': '/start'}
-        )
+        with patch('dialog_manager.bot_utils.client_catalogs_create', lambda path: None):
+            mock_extract_chat_data.return_value = (123, '/start')
+            mock_screen = MockScreenManager.return_value
+            mock_screen.current_screen.return_value.run.return_value = [("Welcome to the bot!", None)]
+            mock_screen.current_screen.return_value.next_screen_name = 'next_screen'
     
-        main.command_handler(message)
-    
-        assert len(bot.sent_messages) > 0
-        assert bot.sent_messages[0][1] == "Welcome to the bot!"
-        mock_extract_chat_data.assert_called_once_with(message)
-        mock_screen.current_screen.assert_called_once_with(123, '/start')
-        mock_screen.update_screen.assert_called_once_with(123, 'next_screen')
+            user = User(1, False, 'TestUser')
+            chat = Chat(1, 'private')
+            message = Message(
+                message_id=1,
+                from_user=user,
+                date=None,
+                chat=chat,
+                content_type='text',
+                options={},
+                json_string={'text': '/start'}
+            )
+        
+            main.command_handler(message)
+        
+            assert len(bot.sent_messages) > 0
+            assert bot.sent_messages[0][1] == "Welcome to the bot!"
+            mock_extract_chat_data.assert_called_once_with(message)
+            mock_screen.current_screen.assert_called_once_with(123, '/start')
+            mock_screen.update_screen.assert_called_once_with(123, 'next_screen')
