62b7ed9ddb56eed2f7434ac90c0325cd5e3d8eb7
Egor Romanchenko <egor.gmag@yandex.ru>
2024-06-10 00:49:22 +0700

    token added

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -1,9 +1,7 @@
-# test_bot.py
-import pytest
 from telebot import TeleBot
 from telebot.types import Message
+import pytest
 import main
-
 
 class MockBot(TeleBot):
     def __init__(self, token):
@@ -15,13 +13,14 @@ class MockBot(TeleBot):
 
 @pytest.fixture
 def bot():
-    mock_bot = MockBot("YOUR_BOT_TOKEN")
+    mock_bot = MockBot("7158514154:AAFeFxYJrzWbC_-IHDDk6Y609uI7GgH0gSQ")
     main.bot = mock_bot
     return mock_bot
 
 def test_bot_message(bot):
-    message = Message(message_id=1, from_user=None, date=None, chat=None, content_type='text', json_string={})  # Поправлено здесь
+    message = Message(1, None, None, None, 'text', 'test', None, None, None, None, None, None)
     message.text = '/start'
     main.handle_start(message)
     assert len(bot.sent_messages) > 0
     assert bot.sent_messages[0][1] == "Welcome to the bot!"
+
