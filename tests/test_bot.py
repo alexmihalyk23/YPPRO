f56c75d94f19462168a64a14585fc01ad767489f
k0te1k4 <egor.gmag@yandex.ru>
2024-06-09 17:43:24 +0700

    Add test and ci

---
+++ tests/test_bot.py
@@ -0,0 +1,27 @@
+from telebot import TeleBot
+from telebot.types import Message
+import main
+
+
+class MockBot(TeleBot):
+    def __init__(self, token):
+        super().__init__(token)
+        self.sent_messages = []
+
+    def send_message(self, chat_id, text, **kwargs):
+        self.sent_messages.append((chat_id, text))
+
+@pytest.fixture
+def bot():
+    mock_bot = MockBot("YOUR_BOT_TOKEN")
+    main.bot = mock_bot
+    return mock_bot
+
+def test_bot_message(bot):
+    message = Message(message_id=1, from_user=None, date=None, chat=None, content_type='text', options={})
+    message.text = '/start'
+    main.handle_start(message)
+    assert len(bot.sent_messages) > 0
+    assert bot.sent_messages[0][1] == "Welcome to the bot!"
+
+
