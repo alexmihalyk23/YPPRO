00ca2cda52fd277eceb4a1d59d3ebcf9b8247445
Egor Romanchenko <egor.gmag@yandex.ru>
2024-06-10 03:18:57 +0700

    test app updated[WIP]

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,30 +1,18 @@
-# import pytest
-# import io
-# from app import app
+import pytest
+import io
+from app import app
+from bs4 import BeautifulSoup
 
-# @pytest.fixture
-# def client():
-#     app.config['TESTING'] = True
-#     with app.test_client() as client:
-#         yield client
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
 
-# def test_home(client):
-#     rv = client.get('/')
-#     assert rv.status_code == 200
-#     assert b"Hello, World!" in rv.data
+def test_home(client):
+    rv = client.get('/')
+    assert rv.status_code == 200
+    soup = BeautifulSoup(rv.data, 'html.parser')
+    assert soup.find(class_='progress-bar-label') is not None
 
-# def test_upload_file(client):
-#     data = {
-#         'file': (io.BytesIO(b'my file contents'), 'test.zip')
-#     }
-#     rv = client.post('/', data=data)
-#     assert rv.status_code == 200
-#     assert b"File saved" in rv.data
 
-# def test_progress(client):
-#     rv = client.get('/progress')
-#     assert rv.status_code == 200
-
-# def test_download_file(client):
-#     rv = client.get('/download/test.zip')
-#     assert rv.status_code == 200
