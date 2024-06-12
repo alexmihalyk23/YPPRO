04497bc8256622cbfcb5969a0e14dd89982d8ebf
Egor Romanchenko <egor.gmag@yandex.ru>
2024-06-12 21:05:36 +0700

    fix path_2

--- tests/testZipFile.js
+++ tests/testZipFile.js
@@ -1,30 +1,27 @@
 const puppeteer = require('puppeteer');
-const assert = require('assert');
 const path = require('path');
 
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('file://' + path.resolve(__dirname, 'index.html'));
+
+    // Путь к файлу index.html в папке templates
+    const filePath = `file://${path.join(__dirname, '../templates/index.html')}`;
+    await page.goto(filePath);
 
     const fileInput = await page.$('#zipFile');
     const sendButton = await page.$('#sendButton');
-    const selectButton = await page.$('#selectButton');
 
-    // Создание пустого файла для теста
-    const emptyZipPath = path.resolve(__dirname, 'empty.zip');
-    const fs = require('fs');
-    fs.writeFileSync(emptyZipPath, '');
+    // Проверяем, что кнопка отправки сначала отключена
+    const isDisabled = await page.evaluate(button => button.disabled, sendButton);
+    console.assert(isDisabled, 'Send button should be disabled initially');
 
-    // Загрузка пустого файла
-    await fileInput.uploadFile(emptyZipPath);
+    // Замените на путь к вашему zip файлу
+    await fileInput.uploadFile(path.join(__dirname, 'test.zip'));
 
-    // Проверка состояния кнопок
-    const sendButtonDisabled = await page.evaluate(button => button.disabled, sendButton);
-    const selectButtonText = await page.evaluate(button => button.innerText, selectButton);
-
-    assert.strictEqual(sendButtonDisabled, false, 'Send button should be enabled');
-    assert.strictEqual(selectButtonText, 'File is loaded', 'Select button text should be "File is loaded"');
+    // Проверяем, что кнопка отправки включена после загрузки файла
+    const isEnabled = await page.evaluate(button => !button.disabled, sendButton);
+    console.assert(isEnabled, 'Send button should be enabled after file upload');
 
     await browser.close();
 })();
